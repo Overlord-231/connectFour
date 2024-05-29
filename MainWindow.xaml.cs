4c5a1598590c0fe4b2a075db6090f62bf348ce34
NagyMilan111 <milan.nagy@student.nhlstenden.com>
2024-05-29 14:59:59 +0200

    Created grid for circles

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace connectFour
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int[] BoardSize;
         public MainWindow()
         {
             InitializeComponent();
@@ -25,6 +26,56 @@ namespace connectFour
         {
             NewGame newGame = new NewGame();
             newGame.ShowDialog();
+            if (newGame.DialogResult == true)
+            {
+                BoardSize = newGame.data;
+                Grid DynamicBoard = new Grid();
+                //DynamicBoard.ShowGridLines = true;
+                Board.Children.Clear();
+
+                // Define the Columns
+
+                SolidColorBrush circleOutline = new SolidColorBrush(Colors.Black);
+                Border cellBorder = new Border();
+                cellBorder.BorderBrush = circleOutline;
+                cellBorder.BorderThickness = new Thickness(1);
+
+                for (int i = 0; i < BoardSize[0]; i++)
+                {
+                    ColumnDefinition colDef = new ColumnDefinition();
+                    DynamicBoard.ColumnDefinitions.Add(colDef);
+
+                }
+
+                // Define the Rows
+                for (int i = 0; i < BoardSize[1]; i++)
+                {
+                    RowDefinition rowDef = new RowDefinition();
+                    DynamicBoard.RowDefinitions.Add(rowDef);
+                }
+
+
+
+
+                for (int row = 0; row < BoardSize[1]; row++)
+                {
+                    for (int col = 0; col < BoardSize[0]; col++)
+                    {
+
+                        Ellipse circle = new Ellipse();
+                        circle.Height = 10;
+                        circle.Width = 10;
+                        circle.Stroke = circleOutline;
+                        circle.HorizontalAlignment = HorizontalAlignment.Center;
+                        circle.VerticalAlignment = VerticalAlignment.Center;
+                        Grid.SetRow(circle, row);
+                        Grid.SetColumn(circle, col);
+                        DynamicBoard.Children.Add(circle);
+                    }
+                }
+
+                Board.Children.Add(DynamicBoard);
+            }
         }
     }
 }
--- NewGame.xaml.cs
+++ NewGame.xaml.cs
@@ -19,6 +19,8 @@ namespace connectFour
     /// </summary>
     public partial class NewGame : Window
     {
+        public int[] data;
+
         public NewGame()
         {
             InitializeComponent();
@@ -26,41 +28,49 @@ namespace connectFour
 
         private void B7x6_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [7, 6];
+            DialogResult = true;
         }
 
         private void B5x4_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [5, 4];
+            DialogResult = true;
         }
 
         private void B6x5_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [6, 5];
+            DialogResult = true;
         }
 
         private void B8x7_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [8, 7];
+            DialogResult = true;
         }
 
         private void B9x7_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [9, 7];
+            DialogResult = true;
         }
 
         private void B10x7_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [10, 7];
+            DialogResult = true;
         }
 
         private void B8x8_Click(object sender, RoutedEventArgs e)
         {
-
+            data = [8, 8];
+            DialogResult = true;
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
+            DialogResult = false;
             Close();
         }
     }
