03b06042f30b8d5a548dfbf4d782af1031db04da
Overlord-231 <nagymilan231@gmail.com>
2024-06-03 15:28:25 +0200

    Added win logic, todo: fix the diagonal win logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,8 +22,11 @@ namespace connectFour
         private string[,] Board;
         private bool red = true;
         private bool yellow = false;
-        private Grid DynamicBoard = new Grid();
-        private Grid StartingBoard = new Grid();
+        private Grid DynamicBoard;
+        private Grid StartingBoard;
+        private bool hasItEnded = false;
+        private int redPair;
+        private int yellowPair;
 
         public MainWindow()
         {
@@ -36,50 +39,30 @@ namespace connectFour
             newGame.ShowDialog();
             if (newGame.DialogResult == true)
             {
-                
                 // Create child of the 'Board' grid
                 BoardSize = newGame.data;
                 BoardSize[1]++;
                 InitializeBoard(BoardSize);
-                BoardGrid.Children.Clear();
-                DynamicBoard = new Grid();
-                StartingBoard = new Grid();
-                // Define the Columns
-                for (int i = 0; i < BoardSize[0]; i++)
-                {
-                    ColumnDefinition colDef = new ColumnDefinition();
-                    StartingBoard.ColumnDefinitions.Add(colDef);
-                }
 
-                for (int i = 0; i < BoardSize[0]; i++)
-                {
-                    FirstRow(StartingBoard, i);
-                }
+                redPair = 0;
+                yellowPair = 0;
 
-                // Define the Rows
-                for (int i = 0; i < BoardSize[1]; i++)
-                {
-                    RowDefinition rowDef = new RowDefinition();
-                    StartingBoard.RowDefinitions.Add(rowDef);
-                }
-                
-                // Add the empty circles to the board with borders
-                for (int row = 1; row < BoardSize[1]; row++)
-                {
-                    for (int col = 0; col < BoardSize[0]; col++)
-                    {
-                        CreateCircles(StartingBoard, row, col);
-                    }
-                }
-
+                StatusDisplay.Text = "Red starts!";
                 DynamicBoard = StartingBoard;
+                hasItEnded = false;
                 BoardGrid.Children.Add(StartingBoard);
             }
         }
 
         private void Reset_click(object sender, RoutedEventArgs e)
         {
+            StatusDisplay.Text = "Red starts!";
             BoardGrid.Children.Clear();
+            InitializeBoard(BoardSize);
+            BoardGrid.Children.Add(StartingBoard);
+            hasItEnded = false;
+            redPair = 0;
+            yellowPair = 0;
         }
 
         private void CreateCircles(Grid grid, int row, int column)
@@ -99,7 +82,7 @@ namespace connectFour
             };
             Grid.SetRow(border, row);
             Grid.SetColumn(border, column);
-            
+
             grid.Children.Add(border);
         }
 
@@ -122,72 +105,235 @@ namespace connectFour
         {
             Button button = sender as Button;
             int column = Grid.GetColumn(button);
-            
-            if (red)
+            if (!hasItEnded)
             {
-                for (int i = BoardSize[1]-1; i > 0; i--)
-                {
-                    if (Board[column, i] == "o")
-                    {
-                        Board[column, i] = "r";
-                        red = false;
-                        yellow = true;
-                        
-                        // Find the ellipse in the grid and change its color to red
-                        var border = FindChildAt(DynamicBoard, i, column) as Border;
-                        if (border != null && border.Child is Ellipse ellipse)
-                        {
-                            ellipse.Fill = new SolidColorBrush(Colors.Red);
-                        }
-
-                        BoardGrid.Children.Clear();
-                        BoardGrid.Children.Add(DynamicBoard);
-                        break;
-                    }
-                }
-
                 if (red)
                 {
-                    MessageBox.Show("Invalid move!");
+                    for (int i = BoardSize[1] - 1; i > 0; i--)
+                    {
+                        if (Board[column, i] == "o")
+                        {
+                            Board[column, i] = "r";
+                            red = false;
+                            yellow = true;
+
+                            // Find the ellipse in the grid and change its color to red
+                            var border = FindChildAt(DynamicBoard, i, column) as Border;
+                            if (border != null && border.Child is Ellipse ellipse)
+                            {
+                                ellipse.Fill = new SolidColorBrush(Colors.Red);
+                            }
+
+                            StatusDisplay.Text = "Yellow's turn!";
+                            BoardGrid.Children.Clear();
+                            BoardGrid.Children.Add(DynamicBoard);
+                            CheckForWin();
+                            break;
+                        }
+                    }
+
+                    if (red)
+                    {
+                        MessageBox.Show("Invalid move!");
+                    }
+                }
+                else
+                {
+                    for (int i = BoardSize[1] - 1; i < BoardSize[1]; i--)
+                    {
+                        if (Board[column, i] == "o")
+                        {
+                            Board[column, i] = "y";
+                            red = true;
+                            yellow = false;
+
+                            var border = FindChildAt(DynamicBoard, i, column) as Border;
+                            if (border != null && border.Child is Ellipse ellipse)
+                            {
+                                ellipse.Fill = new SolidColorBrush(Colors.Yellow);
+                            }
+
+                            StatusDisplay.Text = "Red's turn!";
+                            BoardGrid.Children.Clear();
+                            BoardGrid.Children.Add(DynamicBoard);
+                            break;
+                        }
+                    }
+
+                    if (yellow)
+                    {
+                        MessageBox.Show("Invalid move!");
+                    }
                 }
             }
             else
             {
-                for (int i = BoardSize[1]-1; i < BoardSize[1]; i--)
-                {
-                    if (Board[column, i] == "o")
-                    {
-                        Board[column, i] = "y";
-                        red = true;
-                        yellow = false;
-
-                        var border = FindChildAt(DynamicBoard, i, column) as Border;
-                        if (border != null && border.Child is Ellipse ellipse)
-                        {
-                            ellipse.Fill = new SolidColorBrush(Colors.Yellow);
-                        }
-                        
-                        BoardGrid.Children.Clear();
-                        BoardGrid.Children.Add(DynamicBoard);
-                        break;
-                    }
-                }
-
-                if (yellow)
-                {
-                    MessageBox.Show("Invalid move!");
-                }
+                MessageBox.Show("The game has ended, click on the New Game or Reset button to start a new one.");
             }
         }
 
         private void CheckForWin()
         {
+            CheckVertically();
+            CheckHorizontally();
+            CheckDiagonally();
+        }
+
+        private void CheckDiagonally()
+        {
+            // ascendingDiagonalCheck 
+            for (int i = 3; i < BoardSize[0]; i++)
+            {
+                for (int j = 0; j < BoardSize[1] - 3; j++)
+                {
+                    if (Board[i, j] == "r" && Board[i - 1, j + 1] == "r" &&
+                        Board[i - 2, j + 2] == "r" && Board[i - 3, j + 3] == "r")
+                    {
+                        MessageBox.Show("Red wins!");
+                        StatusDisplay.Text = "Red wins!";
+                        hasItEnded = true;
+                    }
+                }
+            }
             
+            for (int i = 3; i < BoardSize[0]; i++)
+            {
+                for (int j = 0; j < BoardSize[1] - 3; j++)
+                {
+                    if (Board[i, j] == "y" && Board[i - 1, j + 1] == "y" &&
+                        Board[i - 2, j + 2] == "y" && Board[i - 3, j + 3] == "y")
+                    {
+                        MessageBox.Show("Yellow wins!");
+                        StatusDisplay.Text = "Yellow wins!";
+                        hasItEnded = true;
+                    }
+                }
+            }
+
+            // descendingDiagonalCheck
+            for (int i = 3; i < BoardSize[0]; i++)
+            {
+                for (int j = 3; j < BoardSize[1]; j++)
+                {
+                    if (Board[i, j] == "r" && Board[i - 1, j + 1] == "r" &&
+                        Board[i - 2, j + 2] == "r" && Board[i - 3, j + 3] == "r")
+                    {
+                        MessageBox.Show("Red wins!");
+                        StatusDisplay.Text = "Red wins!";
+                        hasItEnded = true;
+                    }
+                }
+            }
+            
+            for (int i = 3; i < BoardSize[0]; i++)
+            {
+                for (int j = 3; j < BoardSize[1]; j++)
+                {
+                    if (Board[i, j] == "y" && Board[i - 1, j + 1] == "y" &&
+                        Board[i - 2, j + 2] == "y" && Board[i - 3, j + 3] == "y")
+                    {
+                        MessageBox.Show("Yellow wins!");
+                        StatusDisplay.Text = "Yellow wins!";
+                        hasItEnded = true;
+                    }
+                }
+            }
+        }
+
+        private void CheckHorizontally()
+        {
+            for (int col = 0; col < BoardSize[0]; col++)
+            {
+                for (int row = 1; row < BoardSize[1]; row++)
+                {
+                    if (Board[row - 1, col] == "r" && Board[row, col] == "r")
+                    {
+                        redPair++;
+                        if (redPair == 3)
+                        {
+                            MessageBox.Show("Red Wins!");
+                            hasItEnded = true;
+                            StatusDisplay.Text = "Red wins!";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        redPair = 0;
+                    }
+                }
+
+                for (int row = 1; row < BoardSize[1]; row++)
+                {
+                    if (Board[row - 1, col] == "y" && Board[row, col] == "y")
+                    {
+                        yellowPair++;
+                        if (yellowPair == 3)
+                        {
+                            MessageBox.Show("Yellow Wins!");
+                            hasItEnded = true;
+                            StatusDisplay.Text = "Yellow wins!";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        yellowPair = 0;
+                    }
+                }
+            }
+        }
+
+        private void CheckVertically()
+        {
+            for (int row = 0; row < BoardSize[1]; row++)
+            {
+                for (int col = 1; col < BoardSize[0]; col++)
+                {
+                    if (Board[row, col - 1] == "r" && Board[row, col] == "r")
+                    {
+                        redPair++;
+                        if (redPair == 3)
+                        {
+                            MessageBox.Show("Red Wins!");
+                            hasItEnded = true;
+                            StatusDisplay.Text = "Red wins!";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        redPair = 0;
+                    }
+                }
+
+                for (int col = 1; col < BoardSize[0]; col++)
+                {
+                    if (Board[row, col - 1] == "y" && Board[row, col] == "y")
+                    {
+                        yellowPair++;
+                        if (yellowPair == 3)
+                        {
+                            MessageBox.Show("Yellow Wins!");
+                            hasItEnded = true;
+                            StatusDisplay.Text = "Yellow wins!";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        yellowPair = 0;
+                    }
+                }
+            }
         }
 
         private void InitializeBoard(int[] BoardSize)
         {
             Board = new string[BoardSize[0], BoardSize[1]];
+            BoardGrid.Children.Clear();
+            DynamicBoard = new Grid();
+            StartingBoard = new Grid();
             for (int i = 0; i < BoardSize[0]; i++)
             {
                 for (int j = 0; j < BoardSize[1]; j++)
@@ -195,8 +341,39 @@ namespace connectFour
                     Board[i, j] = "o";
                 }
             }
+
+            // Define the Rows
+            for (int i = 0; i < BoardSize[1]; i++)
+            {
+                RowDefinition rowDef = new RowDefinition();
+                StartingBoard.RowDefinitions.Add(rowDef);
+            }
+
+            // Define the Columns
+            for (int i = 0; i < BoardSize[0]; i++)
+            {
+                ColumnDefinition colDef = new ColumnDefinition();
+                StartingBoard.ColumnDefinitions.Add(colDef);
+            }
+
+            //Create the buttons in the first row
+            for (int i = 0; i < BoardSize[0]; i++)
+            {
+                FirstRow(StartingBoard, i);
+            }
+
+            // Add the empty circles to the board with borders
+            for (int row = 1; row < BoardSize[1]; row++)
+            {
+                for (int col = 0; col < BoardSize[0]; col++)
+                {
+                    CreateCircles(StartingBoard, row, col);
+                }
+            }
+
+            DynamicBoard = StartingBoard;
         }
-        
+
         private UIElement FindChildAt(Grid grid, int row, int column)
         {
             return grid.Children
